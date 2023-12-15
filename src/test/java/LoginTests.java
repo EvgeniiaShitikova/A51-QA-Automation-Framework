a8ca22681f83bb8df0beb597d90dcd8acfbf52c2
EvgeniiaShitikova <148499763+EvgeniiaShitikova@users.noreply.github.com>
2023-12-14 22:05:53 -0600

    Smoke and Regression

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,50 +1,39 @@
-import pages.HomePage;
-import pages.LoginPage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import Pages.HomePage;
+import Pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
 
     @Test
-    public void loginValidEmailPasswordTest(){
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        loginPage.provideEmail("evgeniia.shitikova@testpro.io");
-        loginPage.providePassword("TridY6F2");
-        loginPage.clickSubmit();
-        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    public void loginSuccessTest()  throws InterruptedException {
+
+        LoginPage loginPage = new LoginPage(getThreadLocal());
+        HomePage homePage = new HomePage(getThreadLocal());
+
+        loginPage.provideEmail("evgeniia.shitikova@testpro.io")
+                .providePassword("TridY6F2")
+                .clickSubmitBtn();
+
+        Thread.sleep(2000);
+        Assert.assertTrue(homePage.getUserAvatar());
     }
-
     @Test
-    public void loginValidEmailPasswordTestByPageFactory(){
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
+    public void loginInvalidCredentials(){
+        LoginPage loginPage = new LoginPage(getThreadLocal());
 
-        loginPage.provideEmailToLogin("evgeniia.shitikova@testpro.io")
-                .providePasswordToLogin("TridY6F2")
-                .clickSubmitBtnLogin();
-
-        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+        loginPage.provideEmail("")
+                .providePassword("TridY6F2")
+                .clickSubmitBtn();
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
     }
-
     @Test
-    public void loginEmptyEmailPassword() {
+    public void loginInvalidPassword(){
+        LoginPage loginPage = new LoginPage(getThreadLocal());
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        loginPage.provideEmail("evgeniia.shitikova@testpro.io")
+                .providePassword("")
+                .clickSubmitBtn();
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
     }
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,27 +1,39 @@
-package pages;
+package Pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
+
+    @FindBy(css = ".avatar")
+    private WebElement userAvatarIcon;
+    // userAvatarIcon: Represents the user avatar element on the page.
+
+
+    @FindBy(css = ".show.success")
+    private WebElement notification;
+    // notification: Represents the element on the page displaying a notification or message.
+
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
+// This is the constructor of the "HomePage" class that takes a WebDriver object as a parameter
+// and invokes the constructor of the superclass (parent class) with this parameter.
+// The constructor is used to initialize the object of the class when an instance is created.
 
-    // Locators
-
-    private By userAvatarIcon = By.cssSelector("img.avatar");
-
-    //Helpers
-
-    public WebElement getUserAvatar(){ return findElement(userAvatarIcon);
+    public boolean getUserAvatar() {
+        return userAvatarIcon.isEnabled();
     }
+// The "getUserAvatar" method returns a boolean value (true or false)
+// depending on whether the "userAvatarIcon" element is enabled.
+// If the element is enabled, the method returns true; otherwise, it returns false.
 
-    public WebElement hoverPlay() {
-        WebElement play = driver.findElement((By.cssSelector("[data-testid='play-btn']")));
-        actions.moveToElement(play).perform();
-        return wait.until(ExpectedConditions.visibilityOf(play));
+    public boolean notificationText() {
+        findElement(notification);
+        return notification.isDisplayed();
     }
+// It uses the "findElement" method to locate the "notification" element.
+// It returns a boolean value (true or false) depending on whether the "notification" element is displayed on the page.
+// If the element is displayed, the method returns true; otherwise, it returns false.
 }
